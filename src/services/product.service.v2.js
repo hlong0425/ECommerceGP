58e760871d5269dcde5414735aa62e086c95dcd5
hlong0425 <lehoanglong2542000@gmail.com>
2023-09-04 16:50:08 +0700

    Section 17,18: Update Update, Find.. Product API

--- src/services/product.service.v2.js
+++ src/services/product.service.v2.js
@@ -5,23 +5,147 @@ import {
     publishProductByShop,
     findAllPublishForShop,
     unPublishProductByShop,
-    searchProductByUser
+    searchProductByUser,
+    findAllProducts,
+    findProduct
 } from '../models/repositories/product.repo.js';
+
+import { removeUndefinedObject, updateNestedObjectParser } from '../utils/index.js';
+
+import productModel from '../models/product.model.js';
+import { Types } from 'mongoose';
+
+
+/**
+ * 
+ * Main flow: 1. ProductFactory -> Clothing -> Product
+ * 
+ */
+
+class Product {
+    constructor({
+        product_id,
+        product_name,
+        product_thumb,
+        product_description,
+        product_price,
+        product_quantity,
+        product_type,
+        product_shop,
+        product_attributes,
+    }) {
+        this.product_id = product_id;
+        this.product_name = product_name;
+        this.product_thumb = product_thumb;
+        this.product_description = product_description;
+        this.product_price = product_price;
+        this.product_quantity = product_quantity;
+        this.product_type = product_type;
+        this.product_shop = product_shop;
+        this.product_attributes = product_attributes;
+    };
+
+    async createProduct(product_id) {
+        return await ProductModel.product.create({
+            ...this, _id: product_id
+        });
+    };
+
+    async updateProduct(product_id, body){
+        return await ProductModel.product.findByIdAndUpdate(product_id, body, {
+            new: true
+        })
+    }
+}   
+
+// Define sub-class for different product types Clothing
+class Clothing extends Product {
+    async createProduct() {
+        const newClothing = await ProductModel.clothing.create({
+            ...this.product_attributes,
+            product_shop: this.product_shop,
+        });
+        if (!newClothing) throw new BadRequestError('create new Clothing Error');
+
+        const newProduct = await super.createProduct(newClothing._id);
+        if (!newProduct) throw new BadRequestError('create new Clothing Error');
+
+        return newProduct;
+    }
+
+    async updateProduct( product_id, product_shop ){
+        const productBody = this;
+     
+        if (productBody.product_attributes){
+            // update child
+            const { matchedCount } = await productModel.clothing.updateOne({
+                _id: new Types.ObjectId(product_id)
+            }, removeUndefinedObject(productBody.product_attributes), {
+                new: true
+            });
+
+            if (matchedCount === 0) throw new BadRequestError("can't not update") 
+        }
+
+        return await super.updateProduct(product_id, productBody);
+    }
+}
+
+// Define sub-class for different product types Electronic
+class Electronics extends Product {
+    async createProduct() {
+        const newElectronics = await ProductModel.electronics.create({
+            ...this.product_attributes,
+            product_shop: this.product_shop,
+        });
+        if (!newElectronics) throw new BadRequestError('create new Electronics Error');
+
+        const newProduct = await super.createProduct(newElectronics._id);
+        if (!newProduct) throw new BadRequestError('create new Clothing Error');
+
+        return newProduct;
+    }
+}
 
 class ProductFactory {
     /*
         type: 'Clothing'
     */
+    static _productRegistery = { 
+        Electronics ,
+        Clothing 
+    };
+
     static async createProduct( type, payload ) {
-        switch(type) {
-            case 'Electronics': return new Electronics(payload).createProduct();
-            case 'Clothing': return new Clothing(payload).createProduct();
-            default: 
-                throw new BadRequestError(`Invalid Product Types ${type}`);
-        }
+        const productInstance = this._productRegistery[type];
+        if (!productInstance) throw new BadRequestError(`Invalid Product Types ${type}`);
+
+        return new productInstance(payload).createProduct();
     }    
 
     // ------------------------ PUT -------------------------------  
+
+    static async updateProduct({
+        product_id, product_shop, body
+    }) {    
+        if (!body.product_type) {
+            throw new BadRequestError(`product_type is required`);
+        }
+        
+        const productInstance = this._productRegistery[body.product_type];
+        if (!productInstance) {
+            throw new BadRequestError(`invalid product type ${body.product_type}`);
+        }
+        console.log("-------- START ------------");
+
+        const updatedRequest = updateNestedObjectParser(body);
+        console.log("updatedRequest >>", updatedRequest);
+
+        console.log("-------- END ------------");
+
+        return new productInstance(body).updateProduct(product_id, product_shop)
+    }
+
     static async publishProductByShop(product_shop, product_id) {
         return await publishProductByShop({product_shop, product_id});
     }
@@ -29,7 +153,7 @@ class ProductFactory {
     static async unPublishProductByShop(product_shop, product_id) {
         return await unPublishProductByShop({product_shop, product_id});
     }
-
+    
     // ------------------------END-PUT ----------------------------
 
     // ------------------------ QUERY -----------------------------
@@ -48,69 +172,16 @@ class ProductFactory {
         return await searchProductByUser({ keySearch });
     }
 
+    static async findAllProducts({ limit = 50, sort = 'ctime', pageNo = 1, filter = { isPublished: true } }){
+        return await findAllProducts({ limit, sort, filter, pageNo, 
+            select: ['product_name', 'product_price', 'product_thumb'] });
+    }
+
+    static async findProduct({ product_id }){
+        return await findProduct({ product_id, unSelect: ['__v', 'product_variations'] });
+    }
+
     // ------------------------ END-QUERY -------------------------
-
-}
-
-class Product {
-    constructor({
-        product_name,
-        product_thumb,
-        product_description,
-        product_price,
-        product_quantity,
-        product_type,
-        product_shop,
-        product_attributes,
-    }) {
-        this.product_name = product_name;
-        this.product_thumb = product_thumb;
-        this.product_description = product_description;
-        this.product_price = product_price;
-        this.product_quantity = product_quantity;
-        this.product_type = product_type;
-        this.product_shop = product_shop;
-        this.product_attributes = product_attributes;
-    };
-
-    async createProduct(product_id) {
-        return await ProductModel.product.create({
-            ...this, _id: product_id
-        });
-    };
-}   
-
-
-// Define sub-class for different product types Clothing
-class Clothing extends Product {
-    async createProduct() {
-        const newClothing = await ProductModel.clothing.create({
-            ...this.product_attributes,
-            product_shop: this.product_shop,
-        });
-        if (!newClothing) throw new BadRequestError('create new Clothing Error');
-
-        const newProduct = await super.createProduct(newClothing._id);
-        if (!newProduct) throw new BadRequestError('create new Clothing Error');
-
-        return newProduct;
-    }
-}
-
-// Define sub-class for different product types Electronic
-class Electronics extends Product {
-    async createProduct() {
-        const newElectronics = await ProductModel.electronics.create({
-            ...this.product_attributes,
-            product_shop: this.product_shop,
-        });
-        if (!newElectronics) throw new BadRequestError('create new Electronics Error');
-
-        const newProduct = await super.createProduct(newElectronics._id);
-        if (!newProduct) throw new BadRequestError('create new Clothing Error');
-
-        return newProduct;
-    }
 }
 
 export default ProductFactory;
--- src/utils/index.js
+++ src/utils/index.js
@@ -4,6 +4,60 @@ const getInfoData = ({fields = [], object = {}}) => {
     return _.pick(object, fields);
 }
 
+const getSelectData = (select = []) => {
+    return Object.fromEntries(select.map(s => [s, 1]));
+};
+
+const unSelectData = (select = []) => {
+    return Object.fromEntries(select.map(s => [s, 0]));
+};
+
+
+const removeUndefinedObject = (obj) => {
+    Object.keys(obj).forEach(k => {
+        if(!obj[k]){
+            delete obj[k];
+        }
+    })
+}
+
+/// Cách 1:
+
+// const updateNestedObjectParser = (obj = {}) => {
+//     const final = {};
+//     Object.keys(obj).forEach(key => {
+//         if (typeof obj[key] === 'object' && !Array.isArray(obj[key])){
+//             const response = updateNestesdObjectParser(obj[key]);
+//             Object.keys(response).forEach(a => {
+//                 final[`${key}.${a}`] = response[a];
+//             })
+//         }
+//         else {
+//             final[key] = obj[key];
+//         }
+//     })
+//     return final;
+// };
+
+/// Cách 2:
+
+const updateNestedObjectParser = (obj, parent, result = {}) => {
+    Object.keys(obj).forEach(k => {
+      const propName = parent ? `${parent}.${k}` : k
+      if (typeof obj[k] == 'object' && !Array.isArray(obj[k])) {
+        updateNestedObjectParser(obj[k], propName, result)
+      }
+      else {
+        result[propName] = obj[k]
+      }
+    })
+    return result
+}
+
 export {
-    getInfoData
+    getInfoData,
+    getSelectData,
+    unSelectData,
+    removeUndefinedObject,
+    updateNestedObjectParser
 }
